05e7ac056f8b00197294213506488ac9e9e126d4
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-04-20 18:15:07 -0700

    MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,11 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const obj = {};
+    [...str].forEach(char => obj[char] = obj[char] + 1 || 1);
+    const val = Math.max(...Object.values(obj));
+    return Object.keys(obj).find(key => obj[key] === val);;
+}
 
 module.exports = maxChar;
